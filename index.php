b468b0bf3b44635e374b34bd2b0b276bce141615
Federico <fede.toldo96@gmail.com>
2024-03-30 16:29:57 +0100

    fix testi

--- index.php
+++ index.php
@@ -34,11 +34,11 @@
                 <div class="col-lg-5 presentazione-team col-12 d-none d-lg-flex"> <img src="<?php echo get_template_directory_uri() . '/assets/images/picture/Staff_Baby_2022.png';?>" loading=lazy alt="Staff"> </div>
                 <div class="col-lg-5 col-12 offset-lg-2 offset-0 row m-auto">
                     <h2 class="col-12 mb-md-5 mb-3"> Ci presentiamo </h2>
-                    <p class="col-12 mt-md-5 mt-3"> In prestigiosa posizione centrale a Milano Marittima l’Hotel Baby si affaccia sul noto Viale 2 Giugno a pochi passi dalla spiaggia. </p>
-                    <p class="col-12"> La struttura medio piccola offre un ottimo confort per quei viaggiatori che vogliono godersi un soggiorno in puro stile romagnolo accompagnati dal piacere di sentirsi a proprio agio. </p>
+                    <p class="col-12 mt-md-5 mt-3"> L’Hotel Baby B&B è in centro a Milano Marittima, si affaccia sul Viale due Giugno a pochi passi dalla spiaggia. </p>
+                    <p class="col-12"> Da noi puoi trovare il tuo posto al mare, la struttura è medio piccola, ideale per quei viaggiatori che non vogliono vincolarsi alla pensione completa e preferiscono rimanere liberti di scoprire tutte le proposte che offre la nostra meravigliosa località. </p>
                     <div class="col-12 d-lg-none my-5"> <img src="<?php echo get_template_directory_uri() . '/assets/images/picture/Staff_Baby_2022.png'; ?>" loading=lazy alt="Hotel Baby"> </div>
-                    <p class="col-12"> La gestione è familiare e custodisce con passione questa struttura da ormai 20 anni.</p>
-                    <p class="col-12"> Il nostro hotel è il punto di partenza ideale per chi vuole scoprire Milano Marittima con la comodità e l’accoglienza che ti fanno sentire a casa. </p>
+                    <p class="col-12"> Siamo in pieno centro Milano marittima, due passi dal mare e dalla passeggiata serale tra i negozi, dalla struttura inoltre puoi raggiungere Cervia anche a piedi in poco più di 700 metri.</p>
+                    <p class="col-12"> La gestione è famigliare e proprio per questo il nostro impegno è volto nel far sentire a casa i nostri ospiti, custodiamo questa struttura da ormai 20 anni facendo del nostro lavoro una grande passione. </p>
                     <p class="firma">Liliana e Aldo</p>
                 </div>
             </div>
